e62efb7ddf4da730fb1586225209c0edc315ffff
Elijah <elijahcravens46@gmail.com>
2023-09-18 12:48:31 -0600

    Created getComputerChoice Function
    Created the array to hold the game tokens for rock, paper, and
    scissors. Created the function to generate a random number, between
    0 and 2 to generate the computer's choice.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+const gameToken = ['Rock', 'Paper', 'Scissors'];
+
+function getComputerChoice(){
+  let computerChoice = Math.floor(Math.random() * 3);
+
+  console.log(gameToken[computerChoice]);
+}
+
+getComputerChoice();
