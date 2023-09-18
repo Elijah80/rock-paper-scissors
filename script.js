3af573603e7655ae804950198acac1f533641cba
Elijah <elijahcravens46@gmail.com>
2023-09-18 14:51:11 -0600

    Created playRound function
    Included the logic to get a user's input and compare to the computer's
    choice and determine a winner or loser.

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-const gameToken = ['Rock', 'Paper', 'Scissors'];
+let playerSelection = window.prompt('Please enter Rock, Paper, or Scissors:')
 
-function getComputerChoice(){
-  let computerChoice = Math.floor(Math.random() * 3);
+function getComputerChoice() {
+	const gameToken = ['Rock', 'Paper', 'Scissors']
 
-  console.log(gameToken[computerChoice]);
+	return gameToken[Math.floor(Math.random() * 3)]
 }
 
-getComputerChoice();
+function playRound(playerChoice, computerChoice) {
+	let winningMessage = 'You Win! ' + playerChoice + ' beats ' + computerChoice
+	let losingMessage = 'You Lose! ' + computerChoice + ' beats ' + playerChoice
+
+	if (playerChoice.toLowerCase() !== computerChoice.toLowerCase()) {
+		switch (playerChoice.toLowerCase()) {
+			case 'rock':
+				if (computerChoice === 'Scissors') {
+					console.log(winningMessage)
+				} else if (computerChoice === 'Paper') {
+					console.log(losingMessage)
+				}
+				break
+			case 'paper':
+				if (computerChoice === 'Rock') {
+					console.log(winningMessage)
+				} else if (computerChoice === 'Scissors') {
+					console.log(losingMessage)
+				}
+				break
+			case 'scissors':
+				if (computerChoice === 'Paper') {
+					console.log(winningMessage)
+				} else if (computerChoice === 'Rock') {
+					console.log(losingMessage)
+				}
+				break
+			default:
+				console.log('You Tied! Please play again')
+		}
+	} else {
+    console.log('You Tied! Please play again');
+  }
+}
+
+playRound(playerSelection, getComputerChoice())
