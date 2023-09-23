98e7fb8f6e6edd37a6e6cc8843460d0482f4f7de
Elijah <elijahcravens46@gmail.com>
2023-09-23 00:34:27 -0600

    Refactored Code
    Refactored the code so that the button sections and results sections
    are created. These sections will display their respective elements.
    Added styling rules for the buttons and the cards in the results
    section.

--- script.js
+++ script.js
@@ -1,53 +1,45 @@
 const container = document.querySelector('.container')
 const gameTokens = ['Rock', 'Paper', 'Scissors']
-
-gameTokens.forEach(token => {
-	createTokenButtons(token)
-})
+let playerScore = 0
+let computerScore = 0
 
 function getComputerChoice(gameToken) {
 	return gameToken[Math.floor(Math.random() * gameToken.length)]
 }
 
 function playRound(playerChoice, computerChoice) {
-	const winningMessage = 'You Win! ' + playerChoice + ' beats ' + computerChoice
-	const losingMessage = 'The Computer Wins! ' + computerChoice + ' beats ' + playerChoice
-	const tyingMessage = "It's a Tie! The computer chose " + computerChoice + ' and you chose ' + playerChoice
-
 	switch (playerChoice) {
 		case 'Rock':
 			if (computerChoice === 'Scissors') {
-				return winningMessage
+				playerScore++
 			} else if (computerChoice === 'Paper') {
-				return losingMessage
-			} else {
-				return tyingMessage
+				computerScore++
 			}
 			break
 		case 'Paper':
 			if (computerChoice === 'Rock') {
-				return winningMessage
+				playerScore++
 			} else if (computerChoice === 'Scissors') {
-				return losingMessage
-			} else {
-				return tyingMessage
+				computerScore++
 			}
 			break
 		case 'Scissors':
 			if (computerChoice === 'Paper') {
-				return winningMessage
+				playerScore++
 			} else if (computerChoice === 'Rock') {
-				return losingMessage
-			} else {
-				return tyingMessage
+				computerScore++
 			}
 			break
-		default:
-			return 'You and the computer tied!'
 	}
+
+	console.log(playerScore)
+	console.log(computerScore)
 }
 
+function updateScore(playerScore) {}
+
 function createTokenButtons(token) {
+	const btnSection = document.querySelector('.btn-section');
 	const btn = document.createElement('button')
 
 	btn.classList.add('btn')
@@ -55,10 +47,57 @@ function createTokenButtons(token) {
 	btn.setAttribute('value', token)
 	btn.textContent = token
 	btn.addEventListener('click', function () {
-		let computerSelection = getComputerChoice(gameTokens);
+		let computerSelection = getComputerChoice(gameTokens)
 
-		console.log(playRound(btn.value, computerSelection));
+		playRound(btn.value, computerSelection)
 	})
 
-	container.appendChild(btn)
+	btnSection.appendChild(btn);
 }
+
+function createButtonSection() {
+	const buttonSection = document.createElement('div')
+	buttonSection.classList.add('btn-section')
+
+	container.appendChild(buttonSection);
+
+	gameTokens.forEach(token => {
+		createTokenButtons(token)
+	})
+}
+
+function createResultsSection() {
+	const resultsSection = document.createElement('div')
+
+	resultsSection.classList.add('results-section')
+
+	resultsSection.appendChild(createResultsCard('Player'));
+	resultsSection.appendChild(createResultsCard('CPU'));
+
+	container.appendChild(resultsSection)
+}
+
+function createResultsCard(player) {
+	const card = document.createElement('div');
+	const header = document.createElement('h1');
+	const score = document.createElement('p');
+
+	card.classList.add('card');
+	header.classList.add('card-header');
+	score.classList.add('score');
+
+	header.textContent = player + ' Score';
+	score.textContent = 0;
+
+	card.appendChild(header);
+	card.appendChild(score);
+
+	return card;
+}
+
+function game() {
+	createButtonSection();
+	createResultsSection()
+}
+
+game()
