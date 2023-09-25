e63d459210e7ebfce342f9868531b7b3540e3bd5
Elijah <elijahcravens46@gmail.com>
2023-09-25 12:58:32 -0600

    Refactored to display scores and winner
    Refactored code to display a running score and inform the user
    who the winner of the round was and who the overall winner is.

--- script.js
+++ script.js
@@ -8,38 +8,70 @@ function getComputerChoice(gameToken) {
 }
 
 function playRound(playerChoice, computerChoice) {
-	switch (playerChoice) {
-		case 'Rock':
-			if (computerChoice === 'Scissors') {
-				playerScore++
-			} else if (computerChoice === 'Paper') {
-				computerScore++
-			}
-			break
-		case 'Paper':
-			if (computerChoice === 'Rock') {
-				playerScore++
-			} else if (computerChoice === 'Scissors') {
-				computerScore++
-			}
-			break
-		case 'Scissors':
-			if (computerChoice === 'Paper') {
-				playerScore++
-			} else if (computerChoice === 'Rock') {
-				computerScore++
-			}
-			break
-	}
+	const displayWinner = document.querySelector('.winner')
+	const results = document.querySelector('.results-section');
 
-	console.log(playerScore)
-	console.log(computerScore)
+	if (playerScore < 4 && computerScore < 4) {
+		switch (playerChoice) {
+			case 'Rock':
+				if (computerChoice === 'Scissors') {
+					playerScore++
+					displayWinner.textContent = 'You Win!'
+				} else if (computerChoice === 'Paper') {
+					computerScore++
+					displayWinner.textContent = 'Computer Won!'
+				}
+				break
+			case 'Paper':
+				if (computerChoice === 'Rock') {
+					playerScore++
+					displayWinner.textContent = 'You Win!'
+				} else if (computerChoice === 'Scissors') {
+					computerScore++
+					displayWinner.textContent = 'Computer Won!'
+				}
+				break
+			case 'Scissors':
+				if (computerChoice === 'Paper') {
+					playerScore++
+					displayWinner.textContent = 'You Win!'
+				} else if (computerChoice === 'Rock') {
+					computerScore++
+					displayWinner.textContent = 'Computer Won!'
+				}
+				break
+		}
+
+		if (playerChoice === computerChoice) {
+			displayWinner.textContent = 'You Tied!'
+		}
+
+		updateScores(playerScore, computerScore)
+	} else if (playerScore === 4) {
+		results.classList.add('final-winner');
+		results.textContent = 'You are the Winner!';
+	} else if (computerScore === 4) {
+		results.classList.add('final-winner');
+		results.textContent = 'The Computer is the Winner!';
+	}
 }
 
-function updateScore(playerScore) {}
+function updateScores(userScore, cpuScore) {
+	const scores = document.querySelectorAll('#score')
+	let user = document.querySelector('.player-score')
+	let cpu = document.querySelector('.cpu-score')
+
+	scores.forEach(score => {
+		if (score.classList.contains('player-score')) {
+			user.textContent = userScore
+		} else if (score.classList.contains('cpu-score')) {
+			cpu.textContent = cpuScore
+		}
+	})
+}
 
 function createTokenButtons(token) {
-	const btnSection = document.querySelector('.btn-section');
+	const btnSection = document.querySelector('.btn-section')
 	const btn = document.createElement('button')
 
 	btn.classList.add('btn')
@@ -52,14 +84,14 @@ function createTokenButtons(token) {
 		playRound(btn.value, computerSelection)
 	})
 
-	btnSection.appendChild(btn);
+	btnSection.appendChild(btn)
 }
 
 function createButtonSection() {
 	const buttonSection = document.createElement('div')
 	buttonSection.classList.add('btn-section')
 
-	container.appendChild(buttonSection);
+	container.appendChild(buttonSection)
 
 	gameTokens.forEach(token => {
 		createTokenButtons(token)
@@ -68,35 +100,40 @@ function createButtonSection() {
 
 function createResultsSection() {
 	const resultsSection = document.createElement('div')
+	const displayWinner = document.createElement('h1')
 
 	resultsSection.classList.add('results-section')
+	displayWinner.classList.add('winner')
 
-	resultsSection.appendChild(createResultsCard('Player'));
-	resultsSection.appendChild(createResultsCard('CPU'));
+	resultsSection.appendChild(createResultsCard('Player'))
+	resultsSection.appendChild(displayWinner)
+	resultsSection.appendChild(createResultsCard('CPU'))
 
 	container.appendChild(resultsSection)
 }
 
 function createResultsCard(player) {
-	const card = document.createElement('div');
-	const header = document.createElement('h1');
-	const score = document.createElement('p');
+	const card = document.createElement('div')
+	const header = document.createElement('h1')
+	const score = document.createElement('p')
 
-	card.classList.add('card');
-	header.classList.add('card-header');
-	score.classList.add('score');
+	card.classList.add(`${player.toLowerCase()}-card`)
+	header.classList.add('card-header')
+	score.classList.add(`${player.toLowerCase()}-score`)
 
-	header.textContent = player + ' Score';
-	score.textContent = 0;
+	score.setAttribute('id', 'score')
 
-	card.appendChild(header);
-	card.appendChild(score);
+	header.textContent = player + ' Score'
+	score.textContent = 0
 
-	return card;
+	card.appendChild(header)
+	card.appendChild(score)
+
+	return card
 }
 
 function game() {
-	createButtonSection();
+	createButtonSection()
 	createResultsSection()
 }
 
